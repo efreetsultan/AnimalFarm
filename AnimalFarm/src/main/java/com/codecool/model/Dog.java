b0e60cfad7d69307fdc7d35d9758cbfed59d24e1
Beata Reka Bujdoso <beata.reka.bujdoso@codecool.com>
2023-01-10 14:50:56 +0100

    add review to project

--- AnimalFarm/src/main/java/com/codecool/model/Dog.java
+++ AnimalFarm/src/main/java/com/codecool/model/Dog.java
@@ -1,7 +1,9 @@
 package com.codecool.model;
 
+// TODO Dog should extend Mammal
 public class Dog extends Animal{
     DogBreeds breed;
+    // TODO if you have a constructor then it's a better patterns to initialize all values there
     private int holes = 0;
 
     public Dog(int weight,DogBreeds breed) {
@@ -16,6 +18,7 @@ public class Dog extends Animal{
     }
 
     public void digHole() {
+        // TODO 10 is a magic number, i suggest extracting it into a constant (Ctrl Alt C for geekness!)
         if(holes>=10) {
             System.err.println("FU no more holes for you!");
         }
